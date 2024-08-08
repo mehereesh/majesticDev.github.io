a793a70d94a8f59e29eb69c89003d7a4ad146f41
Chintamakula mehereesh <104367902+mehereesh@users.noreply.github.com>
2024-08-08 18:45:41 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+function copyCode() {
+    const codeBlock = document.getElementById('code-block');
+    const textArea = document.createElement('textarea');
+    textArea.value = codeBlock.textContent;
+    document.body.appendChild(textArea);
+    textArea.select();
+    document.execCommand('copy');
+    document.body.removeChild(textArea);
+    alert('Code copied to clipboard!');
+}
